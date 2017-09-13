01cd69725b435058ea6f2dcfc4bded063a544107
Tyler Moses <tmosest@gmail.com>
2017-09-13 18:43:19 -0400

    Assignment 1 complete

---
+++ COP-3014-A1/COP-3014-A1-TOM-10-ZIP/TOM-10-A1.cpp
@@ -0,0 +1,12 @@
+// *****************************************
+// Tyler Moses
+// Assignment 1
+// *****************************************
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "My name is Tyler Moses" << std::endl;
+}
---
+++ COP-3014-A1/COP-3014-A1/main.cpp
@@ -0,0 +1,11 @@
+// *****************************************
+// Tyler Moses
+// Assignment 1
+// *****************************************
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello World!\n";
+    std::cout << "My name is Tyler Moses" << std::endl;
+}
